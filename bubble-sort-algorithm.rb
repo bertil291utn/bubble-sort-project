54ce509de6b480555b7db03e2ac735e220a053b6
Bertil <bertilt4@gmail.com>
2020-04-06 11:21:55 -0500

    Update first method
    - Change file name - Add title - Change left and side in the block

--- bubble-sort-algorithm.rb
+++ bubble-sort-algorithm.rb
@@ -1,3 +1,4 @@
+# first method
 def bubble_sort(array)
   length = array.length
   elem_moved = true
@@ -8,7 +9,6 @@ def bubble_sort(array)
       if array[i] != array[length - 1] # to avoid compare the last item with null
         if array[i] > array[i + 1]
           array[i], array[i + 1] = array[i + 1], array[i]
-          print "#{array}\n"
           elem_moved = true
         end
       end
@@ -21,20 +21,21 @@ array = [2, 4, 200, 1, 9, 18, 20, 1]
 print "Original array #{array}\n "
 print "Sorted array#{bubble_sort(array)}"
 
+#second method
 def bubble_sort_by(list)
-   length = list.length
-    list.select{ |value|
+  length = list.length
+  list.select { |value|
     count = 0
-        while count+1 < length
-          condition = yield(list[count], list[count+1]) > 0 
-          condition ? (list[count], list[count+1] = list[count+1], list[count]) : nil
-            count +=1
-        end     
-    }
-    list
+    while count + 1 < length
+      condition = yield(list[count], list[count + 1]) > 0
+      condition ? (list[count], list[count + 1] = list[count + 1], list[count]) : nil
+      count += 1
+    end
+  }
+  list
 end
 
-bubble_sort_by(["hi","hello","hey"]) do |right, left|
-    left.length - right.length
+array = bubble_sort_by(["hi", "hello", "hey"]) do |left, right|
+  left.length - right.length
 end
-
+print array
