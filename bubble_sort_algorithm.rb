3699375a9d3a4af11caa160065b2c5f44713e796
bertil291utn <bertilt4@gmail.com>
2020-04-06 15:38:32 -0500

    Fix linters errors - Change name file to snake_case - Update `if` for `nest unless` for fist method - Change curly braces for `do` and `when` - Update condition var from `> 0` to `.positive?` element

--- bubble_sort_algorithm.rb
+++ bubble_sort_algorithm.rb
@@ -3,15 +3,14 @@ def bubble_sort(array)
   elem_moved = true
   while elem_moved
     elem_moved = false
-    array.each_with_index {
-      |val, i|
-      if array[i] != array[length - 1]
-        if array[i] > array[i + 1]
-          array[i], array[i + 1] = array[i + 1], array[i]
-          elem_moved = true
-        end
+    array.each_with_index do |_, i|
+      next unless array[i] != array[length - 1]
+
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        elem_moved = true
       end
-    }
+    end
   end
   array
 end
@@ -22,18 +21,18 @@ print "Sorted array#{bubble_sort(array)}"
 
 def bubble_sort_by(list)
   length = list.length
-  list.select { |value|
+  list.select do
     count = 0
     while count + 1 < length
-      condition = yield(list[count], list[count + 1]) > 0
+      condition = yield(list[count], list[count + 1]).positive?
       condition ? (list[count], list[count + 1] = list[count + 1], list[count]) : nil
       count += 1
     end
-  }
+  end
   list
 end
 
-array = bubble_sort_by(["hi", "hello", "hey"]) do |left, right|
+array = bubble_sort_by(%w[hi hello hey]) do |left, right|
   left.length - right.length
 end
 print array
