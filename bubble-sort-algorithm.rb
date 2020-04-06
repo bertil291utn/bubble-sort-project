f6df93473a3092fdd91dd4f2347bbdba95d1b3b8
Bertil <bertilt4@gmail.com>
2020-04-06 14:08:54 -0500

    Remving debugging instructions - Removing them bubble-sort-algorithm file

--- bubble-sort-algorithm.rb
+++ bubble-sort-algorithm.rb
@@ -1,12 +1,11 @@
-# first method
 def bubble_sort(array)
   length = array.length
   elem_moved = true
-  while elem_moved #to keep looping in the array until theres any swap positions
+  while elem_moved
     elem_moved = false
     array.each_with_index {
       |val, i|
-      if array[i] != array[length - 1] # to avoid compare the last item with null
+      if array[i] != array[length - 1]
         if array[i] > array[i + 1]
           array[i], array[i + 1] = array[i + 1], array[i]
           elem_moved = true
@@ -21,7 +20,6 @@ array = [2, 4, 200, 1, 9, 18, 20, 1]
 print "Original array #{array}\n "
 print "Sorted array#{bubble_sort(array)}"
 
-#second method
 def bubble_sort_by(list)
   length = list.length
   list.select { |value|
